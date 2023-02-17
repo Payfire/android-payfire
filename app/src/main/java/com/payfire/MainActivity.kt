1ad75df9d82461ee8abea749384a468c2ef20a53
Georgi Velikov <99119321+georgivelikov-devsearch@users.noreply.github.com>
2023-02-17 20:42:22 +0200

    Add some style

--- app/src/main/java/com/payfire/MainActivity.kt
+++ app/src/main/java/com/payfire/MainActivity.kt
@@ -2,6 +2,7 @@ package com.payfire
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.Window
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -26,10 +27,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
